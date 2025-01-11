56d582afaa78d303155cb18bcdf33cc745344944
Szabolcs Juhász <juhaszszabolcs90@gmail.com>
2025-01-11 13:13:14 +0100

    increase calendar size

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,30 @@ const selectedDate = ref(null);
 
 </script>
 <template>
-  <div>
-    <VCalendar />
+  <div class="calendar-container">
     <VDatePicker v-model="selectedDate" />
   </div>
 </template>
 
 <style scoped>
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+
+:global(.vc-day) {
+  width: 120px;
+  height: 80px;
+}
+
+.calendar-container {
+  width: 100vw;
+  height: 100vh;
+  padding: 50px;
+  display: flex;
+  justify-content: center;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { setupCalendar, Calendar, DatePicker } from 'v-calendar';
-import 'v-calendar/style.css';
+import 'v-calendar/dist/style.css';
 
 const app = createApp(App);
 app.use(setupCalendar, {});

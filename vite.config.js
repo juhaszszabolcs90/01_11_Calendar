0499ce2f39509e1fa7d8c035eed31b580d98f164
Szabolcs Juhász <juhaszszabolcs90@gmail.com>
2025-01-11 13:49:58 +0100

    update docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/01_11_Calendar/',
+  build: {
+      outDir: 'docs',
+      emptyOutDir: true
+    }
 })
